c78b214d2c75ec6823c79fec6984904e93a785b3
chzhpro <36259039+chzhpro@users.noreply.github.com>
2018-02-09 15:49:48 +0800

    修改repo参数

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 ﻿var _config = {
     blog_name       : 'Mr. Zhang\'s Blog',         // 博客名称
     owner           : 'chzhpro',           // github 用户名
-    repo            : 'chzhpro.github.com',// github 中对应项目名
+    repo            : 'blog.github.com',// github 中对应项目名
     //duoshuo_id      : 'hello1234',            // 在第三方评论插件多说申请站点的子域名
     access_token : 'e3e1b966e92940ff838c'+'ce0df8d043e59388c2fc',       
      // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
